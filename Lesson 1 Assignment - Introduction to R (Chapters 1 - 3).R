2f16e930b876bbe97d8f5fb5364791b955b0316d
Jeff Larson <jeff_larson@byu.edu>
2022-04-12 09:51:49 -0600

    Commit from RStudio

---
+++ Lesson 1 Assignment - Introduction to R (Chapters 1 - 3).R
@@ -0,0 +1,132 @@
+# Lesson 1 Assignment - Introduction to R (Chapters 1 - 3)
+
+# Your assignment is to write the commands instructed in the comments below. To run your
+# commands, simply hit Ctrl+Enter (command+return on a MAC) when the cursor is on that 
+# command line. You can also type commands directly into the Console below, but you must
+# save them in this file for your assignment.
+
+
+# 1. Assign the variable x with a value of 10
+
+
+# 2. Print out x
+
+
+# 3. Assign the variable y with a value of 180
+
+
+# 4. Calculate y / x
+
+
+# 5. Create variable z with value TRUE
+
+
+# 6. Create variable a with value "nice"
+
+
+# 7. Try adding a and x
+
+
+# 8. This doesn't work because one of these is a non-numeric argument. Check the class of 
+# each variable you've created to see which isn't numeric.
+# (https://campus.datacamp.com/courses/free-introduction-to-r/chapter-1-intro-to-basics-1?ex=8)
+
+
+# 9. Let's create our first vector. Name it v1 and have it contain numbers 1 through 9. 
+
+
+# 10. Print out v1
+
+
+# 11. Calculate the sum of the values in v1
+
+
+# 12. Create a new vector, v2, by squaring v1
+
+
+# 13. Calculate the sum of the values in v2
+
+
+# 14. We believe that the sum of v2 is larger than the sum of v1. Confirm this using 
+# greater than/less than symbols.
+
+
+# 15. Calculate how much bigger the sum of v2 is than the sum of v1
+
+
+# 16. Create a new vector called pts that contains these values in order:
+# 111, 106, 105, 102, 100
+
+
+# 17. Calculate the mean of pts. (HINT: The function *mean()* calculates the mean of 
+# a vector within the parentheses.)
+
+
+# 18. Use brackets to output the 3rd value in pts
+
+
+# 19. Create matrix named m1 with 7 columns and 4 rows. The matrix should contain the 
+# values 1 to 28. Row 1 should have the numbers 1 to 7. (HINT: You may use either ncol or 
+# nrow or both to specify the matrix dimensions.)
+# (https://campus.datacamp.com/courses/free-introduction-to-r/chapter-3-matrices-3?ex=1)
+
+
+# 20. Select the 3rd value in the 2nd row of m1
+# https://campus.datacamp.com/courses/free-introduction-to-r/chapter-3-matrices-3?ex=8
+
+
+# 21. Matrix m1 represents the month of February (when it starts on a Sunday). However, 
+# we should add column and row titles. First create a vector of day labels. Call it days 
+# and make it contain "Su", "M", "T", "W", "Th", "F", "Sa".
+
+
+# 22. Now make another vector named wk that contains "wk1", "wk2", "wk3", "wk4".
+
+
+# 23. Now add days as the column names and wk as the row names.
+# (https://campus.datacamp.com/courses/free-introduction-to-r/chapter-3-matrices-3?ex=3)
+
+
+# 24. Look at your new matrix
+
+
+# 25. We want to add the first week of March... create a new vector named mar containing 
+# values 1 through 7.
+
+
+# 26. Now add mar onto m1 in the appropriate place using rbind
+# (https://campus.datacamp.com/courses/free-introduction-to-r/chapter-3-matrices-3?ex=6)
+
+
+# 27. Look at your new matrix
+
+
+# 28. Create a vector called "Wednesdays" that includes the Wednesday dates from m1. 
+# (Select column 4 from m1.)
+
+
+#29. Create a vector called "Fridays" that includes the Friday dates from m1. (Select
+# the "F" column from m1.)
+
+
+#30. Show the third week of February by selecting "wk3" from m1.
+
+
+#31. Check whether the entries from "wk1" of m1 are equivalent to "mar" from m1.
+# (We know they are equivalent, so we should see a vector of 7 TRUEs.)
+
+
+#32. Create a vector called "HiSat" that indicates whether the Saturday date is greater
+# than 14. (It should have 5 TRUE/FALSE entries.)
+
+
+#33. Use HiSat to select the rows of m1 where the Saturday date is greater than 14.
+
+
+#34. Create a vector called "LowSun" that indicates whether the Sunday date is less 
+# than or equal to 14. 
+
+
+#35. Use LowSun to select the rows of m1 where the Sunday date is less than or equal
+# to 14.
+
