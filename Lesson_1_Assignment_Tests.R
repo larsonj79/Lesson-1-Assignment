ab6b3c6af1c60d8f9c6ed3d131f08570749beaca
Jeff Larson <jeff_larson@byu.edu>
2022-04-12 14:35:50 -0600

    Commit from RStudio

--- Lesson_1_Assignment_Tests.R
+++ Lesson_1_Assignment_Tests.R
@@ -5,18 +5,120 @@ library(testthat)
 # you can have multiple tests for each question
 
 
-test_that("Q1", {
+test_that("Q1 (visible)", {
   
   expect_equal(x, 10) 
-
+  
 })
 
-test_that("second (invisible)", {
-
-  expect_true(is.character(myString))
-  expect_true(nchar(myString) > 2)
-
+test_that("Q3 (visible)", {
+  
+  expect_equal(y, 180) 
+  
 })
+
+test_that("Q4 (visible)", {
+  
+  expect_equal(w, 18) 
+  
+})
+
+test_that("Q5 (visible)", {
+  
+  expect_equal(z, "TRUE") 
+  
+})
+
+test_that("Q6 (visible)", {
+  
+  expect_equal(a, "nice")
+  
+})
+
+test_that("Q7 (visible)", {
+  
+  expect_equal(b, "character")
+  
+})
+
+test_that("Q8 (visible)", {
+  
+  expect_equal(v1[1], 1)
+  expect_equal(v1[2], 2)
+  expect_equal(v1[3], 3)
+  expect_equal(v1[5], 5)
+  expect_equal(v1[8], 8)
+  
+})
+
+test_that("Q10 (visible)", {
+  
+  expect_equal(sumv1, 45)
+  
+})
+
+test_that("Q11 (visible)", {
+  
+  expect_equal(v2[1], 1)
+  expect_equal(v2[2], 4)
+  expect_equal(v2[3], 9)
+  expect_equal(v2[5], 25)
+  expect_equal(v2[8], 64)
+  
+})
+
+test_that("Q12 (visible)", {
+  
+  expect_equal(sumv12, 285)
+  
+})
+
+test_that("Q13 (visible)", {
+  
+  expect_equal(valtest, TRUE)
+  
+})
+
+test_that("Q14 (visible)", {
+  
+  expect_equal(diffv2v1, 240)
+  
+})
+
+test_that("Q15 (visible)", {
+  
+  expect_equal(pts[1], 111)
+  expect_equal(pts[2], 106)
+  expect_equal(pts[3], 105)
+  expect_equal(pts[4], 102)
+  expect_equal(pts[5], 100)
+  
+})
+
+test_that("Q16 (visible)", {
+  
+  expect_equal(meanpts, 104.8, tolerance = 1e-1)
+  
+})
+
+test_that("Q17 (visible)", {
+  
+  expect_equal(pts3, 105)
+  
+})
+
+test_that("Q18 (visible)", {
+  
+  expect_equal(m1[1,1], 1)
+  
+})
+
+
+
+
+
+
+
 
 test_that("third", {
 
